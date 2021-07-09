b80e9a42d90b8b377d3d8f9a164920f8e33d166e
Abilat01 <Abilat01@yandex.ru>
2021-07-09 20:13:28 +0300

    Основная логика

--- SleepTime/ViewController.swift
+++ SleepTime/ViewController.swift
@@ -8,12 +8,54 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    var array = [Date]()
+    
+    
+    
+    lazy var dateFormatter: DateFormatter = {
+        let formater = DateFormatter()
+        formater.dateStyle = .short
+        formater.timeStyle = .short
+        return formater
+    }()
+    
+    
+    @IBAction func addPressed(_ sender: UIBarButtonItem) {
+        let date = Date()
+        array.append(date)
+        tableView.reloadData()
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "Cell")
     }
-
-
 }
 
+//MARK: - UITableViewDataSource
+
+extension ViewController: UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return array.count
+    }
+    
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell")
+        
+        let date = array[indexPath.row]
+        cell?.textLabel?.text = dateFormatter.string(from: date)
+        
+        return cell!
+    }
+    
+    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        return "Время отхода ко сну"
+    }
+    
+    
+}
